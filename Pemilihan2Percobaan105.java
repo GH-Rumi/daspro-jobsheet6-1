eab859cfeac5d82d826910c121a4b6a9de24e2aa
rumi <user@LAPTOP-0IU79HM9git config --global user.name rumigit onfig --get-all user.namegit onfig --get-all user.nameGH-Rumigit onfig --get-all user.name-- get-all GH-Rumi!git config --global user.email user@LAPTOP-0IU79HM9>
2024-10-04 13:59:58 +0700

    Modifikasi

--- Pemilihan2Percobaan105.java
+++ Pemilihan2Percobaan105.java
@@ -7,14 +7,21 @@ import java.util.Scanner;
 	        Scanner Absen = new Scanner(System.in);
 	        int tahun;
 	
-	        System.out.print("Masukkan tahun: ");
-	        tahun = Absen.nextInt();
-	    
-	        if ((tahun % 4) == 0) {
-	            if ((tahun % 100) != 0)
-	            System.out.println("Tahun Kabisat");
-        } else
-	            System.out.println("Bukan Tahun Kabisat");
-	    }
-	    
+			System.out.print("Masukkan Tahun : ");
+			tahun = Absen.nextInt();
+	
+			if (tahun % 4 == 0) {
+				if (tahun % 100 == 0) {
+					System.out.println("Tahun Kabisat");
+				} else {
+					if (tahun % 400 == 0) {
+						System.out.println("Tahun Kabisat");
+					} else {
+						System.out.println("Bukan Tahun Kabisat");
+					}
+				}
+			} else {
+				System.out.println("Bukan Tahun Kabisat");
+			}
+		}
 	}
